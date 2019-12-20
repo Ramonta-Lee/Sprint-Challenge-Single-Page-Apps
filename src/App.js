42a59843c9904684115a9502515dc830dc37621b
unknown <viola4lfe@gmail.com>
2019-12-20 13:31:43 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList";
+import SearchForm from "./components/SearchForm";
+import axios from "axios";
 
 
 export default function App() {
+
+  const [characters, setCharacters] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    // TODO: Add API Request here - must run in `useEffect`
+    axios
+      .get(`https://rickandmortyapi.com/api/character/?name=${search}`)
+      .then(response => {
+        console.log(response.data.results);
+        
+          setCharacters(response.data.results);
+      })
+      .catch(error => console.log(error));
+
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  }, [search]);
+
+  const handleSearchChange = event => {
+    setSearch(event.target.value);
+  };
+
+  if (!characters) {
+    return <h2>...loading</h2>;
+  }
   return (
     <main>
       <Header />
+      <SearchForm characters={characters} handleSearchChange={handleSearchChange} />
+      <div>
+      <CharacterList characters={characters} />
+      </div>
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,34 @@
 import React from "react";
+import {
+  Card,
+  CardBody,
+  Button,
+  CardTitle,
+  CardImg,
+  CardSubtitle
+} from "reactstrap";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  // return <span>todo: character</span>;
+  // console.log("I am props", props.character);
+  const { name, status, gender, species, image } = props.character;
+  return (
+    <div>
+      <Card className="mt-4">
+        <CardTitle>Name: {name}</CardTitle>
+        <CardImg
+          top
+          width="100%"
+          src={image}
+          alt="Photo of Rick and Morty characters"
+        />
+        <CardBody>
+          <CardSubtitle>Status: {status}</CardSubtitle>
+          <CardSubtitle>Species: {species}</CardSubtitle>
+          <CardSubtitle>Gender: {gender}</CardSubtitle>
+          <Button color="success">More</Button>{" "}
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
